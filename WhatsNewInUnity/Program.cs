bdce8df436f21e6a337bb7b110befcc97066806d
Anthony <8753187+bigfoott@users.noreply.github.com>
2019-02-07 11:45:58 -0500

    moved web stuff out of loop and prevented tweeting when version.txt not found

--- WhatsNewInUnity/Program.cs
+++ WhatsNewInUnity/Program.cs
@@ -24,13 +24,14 @@ namespace WhatsNewInUnity
                 string[] lines = File.ReadAllLines("auth.txt");
                 Auth.SetUserCredentials(lines[0], lines[1], lines[2], lines[3]);
 
+                var web = new HtmlWeb();
+                string url = "https://unity3d.com/unity/whats-new";
+                
                 while (true)
                 {
-                    var web = new HtmlWeb();
-                    string url = "https://unity3d.com/unity/whats-new";
                     var doc = web.Load(url);
                     string ver = doc.DocumentNode.SelectSingleNode("//h1").InnerText;
-                    if (!File.Exists("version.txt")) File.WriteAllText("version.txt", "");
+                    if (!File.Exists("version.txt")) File.WriteAllText("version.txt", ver);
                     if (File.ReadAllText("version.txt") != ver)
                     {
                         Tweet.PublishTweet(ver + " is out!\n\nView the changelog here: " + url + "/" + ver.Split(' ')[1]);
