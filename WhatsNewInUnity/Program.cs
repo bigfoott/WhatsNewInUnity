53e8ccdaff759e0165419abf3561f0c9d533f35f
Anthony <8753187+bigfoott@users.noreply.github.com>
2019-02-08 20:40:11 -0500

    added support for alpha and beta versions of unity

--- WhatsNewInUnity/Program.cs
+++ WhatsNewInUnity/Program.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Tweetinvi;
+using System.Xml;
+using System.Net;
 
 namespace WhatsNewInUnity
 {
@@ -25,7 +27,7 @@ namespace WhatsNewInUnity
                 string[] lines = File.ReadAllLines("auth.txt");
                 Auth.SetUserCredentials(lines[0], lines[1], lines[2], lines[3]);
 
-                var web = new HtmlWeb();
+                HtmlWeb web;
                 string url = "https://unity3d.com/unity/whats-new";
 
                 Console.WriteLine("[" + DateTime.Now + "] Beginning loop.");
@@ -34,20 +36,47 @@ namespace WhatsNewInUnity
                     Thread.Sleep(600000); // 10 minutes
 
                     HtmlDocument doc = null;
-
-                    try { doc = web.Load(url); }
+                    try
+                    {
+                        web = new HtmlWeb();
+                        doc = web.Load(url);
+                    }
                     catch { }
 
-                    if (doc == null)
-                        continue;
-                    
-                    string ver = doc.DocumentNode.SelectSingleNode("//h1").InnerText;
-                    if (!File.Exists("version.txt")) File.WriteAllText("version.txt", ver);
-                    if (File.ReadAllText("version.txt") != ver)
+                    string[] rss = new string[0];
+                    try
                     {
-                        Tweet.PublishTweet(ver + " is out!\n\nView the changelog here: " + url + "/" + ver.Split(' ')[1]);
-                        Console.WriteLine("[" + DateTime.Now + "] Tweeted update " + ver);
-                        File.WriteAllText("version.txt", ver);
+                        using (var wc = new WebClient())
+                        {
+                            rss = wc.DownloadString("https://unity3d.com/unity/beta/latest.xml").Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                        }
+                    } 
+                    catch { }
+
+                    if (doc != null)
+                    {
+                        string ver = doc.DocumentNode.SelectSingleNode("//h1").InnerText;
+                        if (!File.Exists("version.txt")) File.WriteAllText("version.txt", ver);
+                        if (File.ReadAllText("version.txt") != ver)
+                        {
+                            Tweet.PublishTweet(ver + " is out!\n\nView the changelog here: " + url + "/" + ver.Split(' ')[1]);
+                            Console.WriteLine("[" + DateTime.Now + "] Tweeted update " + ver);
+                            File.WriteAllText("version.txt", ver);
+                        }
+                    }
+
+                    if (rss.Length != 0)
+                    {
+                        string ver = rss.First(s => s.Contains("<title>"));
+                        ver = ver.Split(new string[] { "<title>" }, StringSplitOptions.None)[2].Split(new string[] { "</title>" }, StringSplitOptions.None)[0].Split(' ')[1];
+
+                        if (!File.Exists("version-alphabeta.txt")) File.WriteAllText("version-alphabeta.txt", ver);
+                        if (File.ReadAllText("version-alphabeta.txt") != ver)
+                        {
+                            Tweet.PublishTweet(ver + " is out!\n\nView the changelog here: " + url + "/" + ver.Split(' ')[1]);
+                            Console.WriteLine("[" + DateTime.Now + "] Tweeted update " + ver);
+                            File.WriteAllText("version-alphabeta.txt", ver);
+                        }
                     }
                 }
             }
